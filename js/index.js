de4e0ff3c272df964acce8341401e607613c320d
jordi <a18jorgornei@inspedralbes.cat>
2020-12-15 21:40:16 +0100

    tot be, intregat la creacio de nova experiencia

--- js/index.js
+++ js/index.js
@@ -152,7 +152,7 @@ window.onload = function () {
         });
     })
 
-
+    //////// Start Print Experiencies Jordi
     function printExperiencies(baseDades) {
         document.getElementById("content").innerHTML="";
         let htmlExperiences = '<h2 id="titolExperiencies">Experiencies</h2>';
@@ -185,6 +185,7 @@ window.onload = function () {
             }
         });
         htmlExperiences += '</div>';
+        htmlExperiences += '<button id="newExp">Nova Experiencia</button>';
         // document.getElementById('enunciat').insertAdjacentHTML('afterEnd', htmlExperiences);
         document.getElementById("content").innerHTML=htmlExperiences;
     
@@ -244,6 +245,138 @@ window.onload = function () {
             })
         }
     }
+    //////// Finish Print Experiencies Jordi
+    //////////////////// Start Nova Experiencia Jordi
+    // Printar formulari
+    function creatFormExp() {
+        let crearFormNovaExperiencia =
+        `
+        <div id="formNewExp">
+            <h2>Nova Experiencia</h2>
+
+            <label for="titolExp">Titol: </label>
+            <input type="text" name="titolExp" id="titolExp"><br>
+
+            <label for="dataExp">Data: </label>
+            <input type="date" name="dataExp" id="dataExp"><br>
+
+            <label for="textExp">Text:</label>
+            <textarea id="textExp" name="textExp" rows="4" cols="50"></textarea><br>
+
+            <div id="categoriaExp">
+                <input type="radio" id="r1" name="categoriaExp" value="Aventures" checked="checked">
+                <label for="Aventures">Aventures</label><br>
+                <input type="radio" id="r2" name="categoriaExp" value="Familiar">
+                <label for="Familiar">Familiar</label><br>
+                <input type="radio" id="r3" name="categoriaExp" value="Historic">
+                <label for="Historic">Historic</label><br>
+                <input type="radio" id="r4" name="categoriaExp" value="Muntanyisme">
+                <label for="Muntanyisme">Muntanyisme</label><br>
+                <input type="radio" id="r5" name="categoriaExp" value="Romantic">
+                <label for="Romantic">Romantic</label><br>
+            </div>
+
+            <button id="btnCrearExp">Crear</button>
+        </div>
+        `;
+
+        document.getElementById('newExp').insertAdjacentHTML('afterEnd', crearFormNovaExperiencia);
+    }
+    // Remove caracters specials
+    function escapeHtml(text) {
+        let map = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#039;'
+        };
+        
+        return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+    }
+    // Validacio per php
+    function testInput(data) {
+        data = data.replace(/\\/g," ");
+        data = data.trim();
+        data = escapeHtml(data);
+        return data;
+    }
+    // Validacio per js
+    function validateForm() {
+        let titol = document.getElementById('titolExp').value;
+        let data = document.getElementById('dataExp').value;
+        let text = document.getElementById('textExp').value;
+        let categoria = function(){
+            if (document.getElementById('r1').checked) {
+                return document.getElementById('r1').value;
+            } else if (document.getElementById('r2').checked) {
+                return document.getElementById('r2').value;
+            } else if (document.getElementById('r3').checked) {
+                return document.getElementById('r3').value;
+            } else {
+                return document.getElementById('r4').value;
+            }
+        }
+        let imatge = "https://picsum.photos/400/300";
+
+        if (titol == "") {
+            alert("Cal omplir el Titol");
+            return false;
+        } else if (data == "") {
+            alert("Cal omplir la Data");
+            return false;
+        } else if (text == "") {
+            alert("Cal omplir el Text");
+            return false;
+        } else {
+            console.log(testInput(titol));
+            console.log(data);
+            console.log(testInput(text));
+            console.log(categoria());
+            console.log(imatge);
+
+            let experiencia = [testInput(titol), data, testInput(text), categoria(), imatge];
+            return experiencia;
+        }
+    }
+    // Crear Stringify
+    function expToJson(titol, data, text, categoria, imatge) {
+        let novaExperiencia = new Map();
+        novaExperiencia['titol'] = titol;
+        novaExperiencia['data'] = data;
+        novaExperiencia['text'] = text;
+        novaExperiencia['imatge'] = imatge;
+        novaExperiencia['coordenades'] = '0, 0';
+        novaExperiencia['categoria'] = categoria;
+        novaExperiencia['likes'] = 0;
+        novaExperiencia['dislikes'] = 0;
+        novaExperiencia['estat'] = 'esborrany';
+        return novaExperiencia;
+    }
+
+    // Listeners
+    // Nova Experiencia
+    // document.getElementById("newExp").addEventListener("click", function(){
+    //     creatFormExp();
+    // })
+    document.addEventListener('click',function(e){
+        if(e.target && e.target.id == 'newExp'){
+            creatFormExp();
+            document.getElementById("newExp").disabled = true;
+        }
+    });
+    // Crear Nova Experiencia
+    document.addEventListener('click',function(e){
+        if(e.target && e.target.id == 'btnCrearExp'){
+            if (validateForm()) {
+                let experiencia = validateForm();
+                console.log(expToJson(experiencia[0], experiencia[1], experiencia[2], experiencia[3], experiencia[4]));
+                document.getElementById("formNewExp").style.display = "none";
+                document.getElementById("newExp").disabled = false;
+            }
+        }
+    });
+    //////////////////// Finish Nova Experiencia Jordi
 
     function updateLikes (idUsu, likes, dislikes) {
         axios.get("http://labs.iam.cat/~a18pabgombra/CallejerosViajeros/database/experiencias/updateLikes.php",{
