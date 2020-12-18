efe81d283870cadac4d51c7c2fd5b743fcb04a86
PabloGomezB <73338310+PabloGomezB@users.noreply.github.com>
2020-12-18 09:03:14 +0100

    bug en headers arreglado

--- database/experiencias/reportarExp.php
+++ database/experiencias/reportarExp.php
@@ -1,11 +1,10 @@
 <?php 
-    header("Access-Control-Aloow-Origin: *");
-    header("Content-Type: text/html; charset=utf8");
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: text/html; charset-utf-8');
+require_once('../Experiencia.php');
 
-    require_once('../Experiencia.php');
-
-    $exp = new Experiencia ();
-    $exp->reportarExp($_REQUEST['idUsu']);
-    $experiencias = $exp->mostrarTot();
-    echo json_encode($experiencias);
+$exp = new Experiencia ();
+$exp->reportarExp($_REQUEST['idUsu']);
+$experiencias = $exp->mostrarTot();
+echo json_encode($experiencias);
 ?>
