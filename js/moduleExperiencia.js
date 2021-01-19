582ac7dc887510545514ab52f63454d21026a3e0
PabloGomezB <73338310+PabloGomezB@users.noreply.github.com>
2021-01-19 10:18:08 +0100

    LAST COMMIT

--- js/moduleExperiencia.js
+++ js/moduleExperiencia.js
@@ -212,37 +212,11 @@ var moduleExperiencia = (function () {
                         </div>
                     </div>`
 
-                    
-                    
-                
-
 
                     aux_modal = modal;
                     document.getElementById("divModal").innerHTML = modal;
                     $('#modal').modal();
 
-
-
-
-                    // $('#upload').on('click', function() {
-                    //     var file_data = $('#sortpicture').prop('files')[0];   
-                    //     var form_data = new FormData();                  
-                    //     form_data.append('file', file_data);
-                    //     alert(form_data);                             
-                    //     $.ajax({
-                    //         // url: 'http://labs.iam.cat/~a18jorgornei/upload.php', // point to server-side PHP script
-                    //         url: './upload.php', // point to server-side PHP script 
-                    //         dataType: 'text',  // what to expect back from the PHP script, if anything
-                    //         cache: false,
-                    //         contentType: false,
-                    //         processData: false,
-                    //         data: form_data,                         
-                    //         type: 'post',
-                    //         success: function(php_script_response){
-                    //             alert(php_script_response); // display response from the PHP script, if any
-                    //         }
-                    //     });
-                    // });
 
                     /////////////////////////////////////////////////////////////////
                     //             LISTENERS (like/dislike/editar...)              //
